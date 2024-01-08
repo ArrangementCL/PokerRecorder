0d6746fbf507717d9de189d3cd1bb2593bb269c3
ChenLie <chenlie427@gmail.com>
2024-01-08 11:06:55 +0800

    Create New Project.

---
+++ PokerRecorder/PokerRecorder/PokerRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerRecorderApp.swift
+//  PokerRecorder
+//
+//  Created by mini on 2024/1/8.
+//
+
+import SwiftUI
+
+@main
+struct PokerRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
